3fcafb64e12954620e35240cf0f6a0e5eb4d6ce5
ViacheslavKosh <koshelevskiy.v@gmail.com>
2024-03-15 13:58:37 +0200

    first done

---
+++ task_01.py
@@ -0,0 +1,25 @@
+
+def total_salary(path):
+        try:
+            with open(path, "r", encoding="UTF-8") as arg:
+                lines = [el.strip() for el in arg.readlines()]
+                count = 0
+                total_salary = 0
+                for line in lines:
+                    parts = line.split(',')
+                    salary_int = int(parts[1])
+                    total_salary += salary_int
+                    count += 1
+                average_salary = total_salary//count
+            return total_salary, average_salary, count
+        except FileNotFoundError:
+             print(f'File {path} not found!')
+             return None, None, None
+        except Exception as e:
+             print(f'Have an error: {e}!')
+             return None, None, None
+
+
+# Приклад використання
+total, average, count = total_salary("salary.txt")
+print(f"We have {count} employees. Total salary: {total}. Averege salary is: {average}")   
